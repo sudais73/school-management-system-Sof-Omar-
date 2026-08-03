72d787e426038c4cf3227359b642ab3981a11c95
sudais73 <sudaisman193@gmail.com>
2026-08-03 16:03:01 +0300

    updating front end to use tanstack just for front edn only

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,10 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import neon from './neon-vite-plugin.ts'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), neon, tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(),tailwindcss(), tanstackStart(), viteReact()],
 })
 
 export default config
