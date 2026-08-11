a45f4f588b2f19d9d72a75fffbdc02cefca8c04e
sudais73 <sudaisman193@gmail.com>
2026-08-11 20:15:44 +0300

    Configure TanStack Start for Netlify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,34 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+// import { defineConfig } from 'vite'
+// import { devtools } from '@tanstack/devtools-vite'
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+// import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import viteReact from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
-const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [devtools(),tailwindcss(), tanstackStart(), viteReact()],
-})
+// const config = defineConfig({
+//   resolve: { tsconfigPaths: true },
+//   plugins: [devtools(),tailwindcss(), tanstackStart(), viteReact()],
+// })
 
-export default config
+// export default config
+
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    netlify(),
+    viteReact(),
+  ],
+});
