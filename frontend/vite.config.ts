879cd2faa071ebd3db8e7b471fa0f90e926e22d6
sudais73 <sudaisman193@gmail.com>
2026-08-14 11:19:56 +0300

    pwa icons added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -40,19 +40,19 @@ export default defineConfig({
 
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "/pwa.jpg",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/jpg+xml",
           },
           {
-            src: "/pwa-512x512.png",
+           src: "/pwa1.jpg",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/jpg+xml",
           },
           {
-            src: "/pwa-512x512.png",
+           src: "/pwa-192x192.svg",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/svg+xml",
             purpose: "any maskable",
           },
         ],
